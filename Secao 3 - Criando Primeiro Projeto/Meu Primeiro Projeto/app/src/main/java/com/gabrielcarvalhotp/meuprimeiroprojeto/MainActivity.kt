3a44b141237048ddd72899c67d01e48024e0671f
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2025-05-24 11:37:34 -0300

    set handle button click

--- Secao 3 - Criando Primeiro Projeto/Meu Primeiro Projeto/app/src/main/java/com/gabrielcarvalhotp/meuprimeiroprojeto/MainActivity.kt
+++ Secao 3 - Criando Primeiro Projeto/Meu Primeiro Projeto/app/src/main/java/com/gabrielcarvalhotp/meuprimeiroprojeto/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.gabrielcarvalhotp.meuprimeiroprojeto
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnClickHere: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnClickHere = findViewById<Button>(R.id.btnClickHere)
+        btnClickHere.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnClickHere -> handleBtnClickHere()
+        }
+    }
+
+    private fun handleBtnClickHere() {
+        Toast.makeText(this, R.string.button_click_with_success, Toast.LENGTH_LONG).show()
+    }
+
 }
