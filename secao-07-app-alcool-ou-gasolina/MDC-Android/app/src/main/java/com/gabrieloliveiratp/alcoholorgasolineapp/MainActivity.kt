5d827eaaf655f31b0bd0869a130c7832284a457e
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2025-10-22 19:55:10 -0300

    finish app in MDC-Android

--- secao-07-app-alcool-ou-gasolina/MDC-Android/app/src/main/java/com/gabrieloliveiratp/alcoholorgasolineapp/MainActivity.kt
+++ secao-07-app-alcool-ou-gasolina/MDC-Android/app/src/main/java/com/gabrieloliveiratp/alcoholorgasolineapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.gabrieloliveiratp.alcoholorgasolineapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textAlcoholPrice: TextInputLayout
+    private lateinit var textGasolinePrice: TextInputLayout
+    private lateinit var editAlcoholPrice: TextInputEditText
+    private lateinit var editGasolinePrice: TextInputEditText
+    private lateinit var buttonCalculate: Button
+    private lateinit var textResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,45 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeViewComponents()
+        buttonCalculate.setOnClickListener { calculateTheBestChoice() }
+    }
+
+    private fun initializeViewComponents() {
+        textAlcoholPrice = findViewById(R.id.text_alcohol_price)
+        textGasolinePrice = findViewById(R.id.text_gasoline_price)
+        editAlcoholPrice = findViewById(R.id.edit_alcohol_price)
+        editGasolinePrice = findViewById(R.id.edit_gasoline_price)
+        buttonCalculate = findViewById(R.id.button_calculate)
+        textResult = findViewById(R.id.text_result)
+    }
+
+    private fun calculateTheBestChoice() {
+        val alcoholPrice = editAlcoholPrice.text.toString()
+        val gasolinePrice = editGasolinePrice.text.toString()
+
+        if (!validateFields(alcoholPrice, gasolinePrice)) return
+
+        if (alcoholPrice.toDouble() / gasolinePrice.toDouble() >= 0.7 ) {
+            textResult.text = "Melhor Utilizar Gasolina"
+        } else {
+            textResult.text = "Melhor Utilizar Álcool"
+        }
+    }
+
+    private fun validateFields(alcoholPrice: String, gasolinePrice: String): Boolean {
+        textAlcoholPrice.error = null
+        textGasolinePrice.error = null
+
+        if (alcoholPrice.isEmpty()) {
+            textAlcoholPrice.error = "Preecha o valor do álcool corretamente!"
+            return false
+        }
+        if (gasolinePrice.isEmpty()) {
+            textGasolinePrice.error = "Preecha o valor da gasolina corretamente!"
+            return false
+        }
+        return true
     }
 }
