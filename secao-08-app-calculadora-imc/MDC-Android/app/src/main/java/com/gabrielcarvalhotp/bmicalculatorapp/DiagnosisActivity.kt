0ff9d22045351e8517f64c032fa9ea56f6196f80
Gabriel.Oliveira <gabrieltp2004@gmail.com>
2025-10-24 12:37:57 -0300

    finish bmi calculator app

--- secao-08-app-calculadora-imc/MDC-Android/app/src/main/java/com/gabrielcarvalhotp/bmicalculatorapp/DiagnosisActivity.kt
+++ secao-08-app-calculadora-imc/MDC-Android/app/src/main/java/com/gabrielcarvalhotp/bmicalculatorapp/DiagnosisActivity.kt
@@ -1,12 +1,18 @@
 package com.gabrielcarvalhotp.bmicalculatorapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DiagnosisActivity : AppCompatActivity() {
+
+    lateinit var textWeight: TextView
+    lateinit var textHeight: TextView
+    lateinit var textResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,29 @@ class DiagnosisActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initializeViewComponents()
+
+        val extras = intent.extras!!
+        val weight = extras.getDouble("weight")
+        val height = extras.getDouble("height")
+
+        textWeight.text = "Peso informado: $weight kg"
+        textHeight.text = "Altura informada: $height m"
+
+        val bmi = weight / (height * height)
+
+        textResult.text = when (bmi) {
+            in 0.0..18.5 -> "Baixo"
+            in 18.5..24.9 -> "Normal"
+            in 25.0..29.9 -> "Sobrepeso"
+            in 30.0..39.9 -> "Obesidade"
+            else -> "Vai fazer uma dieta quando?"
+        }
+    }
+
+    private fun initializeViewComponents() {
+        textWeight = findViewById(R.id.text_weight)
+        textHeight = findViewById(R.id.text_height)
+        textResult = findViewById(R.id.text_result)
     }
 }
--- secao-08-app-calculadora-imc/MDC-Android/app/src/main/java/com/gabrielcarvalhotp/bmicalculatorapp/MainActivity.kt
+++ secao-08-app-calculadora-imc/MDC-Android/app/src/main/java/com/gabrielcarvalhotp/bmicalculatorapp/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var buttonCalculate: Button
+    lateinit var editWeight : TextInputEditText
+    lateinit var editHeight : TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +31,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initializeViewComponents() {
         buttonCalculate = findViewById(R.id.button_calculate)
+        editWeight = findViewById(R.id.edit_weight)
+        editHeight = findViewById(R.id.edit_height)
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.button_calculate -> {
-                val intent = Intent(this, DiagnosisActivity::class.java)
+                editWeight.error = null
+                editHeight.error = null
 
+                val weight = editWeight.text.toString().toDoubleOrNull()
+                val height = editHeight.text.toString().toDoubleOrNull()
+
+                if (weight == null) {
+                    editWeight.error = "Preencha o peso corretamente!"
+                }
+
+                if (height == null) {
+                    editHeight.error = "Preencha a altura corretamente!"
+                }
+
+                if (weight == null || height == null) return
+
+                val intent = Intent(this, DiagnosisActivity::class.java)
+                intent.putExtra("weight", weight)
+                intent.putExtra("height", height)
                 startActivity(intent)
             }
         }
